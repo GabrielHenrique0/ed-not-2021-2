73d1f809bf1e1095821727c3355782da74e9cc5a
GabrielHenrique0 <gabrielcaldeira215@gmail.com>
2021-11-12 23:45:49 +0000

    (12/11) Continuação da árvore binária de busca

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -2,7 +2,7 @@
     Classe que representa cada unidade (elemento) da árvore binária de busca
     É dividida em três partes:
     1) Onde fica armezenada a informação relevante para o usuário (data)
-    1) O ponteiro para a subárvore esquerda (left)
+    2) O ponteiro para a subárvore esquerda (left)
     3) O ponteiro para a subárvore direita (right)
 """
 class Node:
@@ -23,12 +23,12 @@ class Node:
       colocados já em ordem.
 """
 class BinarySearchTree:
-    
+
     """
         Construtor da classe
     """
     def __init__(self):
-        self.__root = None         # Raiz da árvore
+        self.__root = None      # Raiz da árvore
 
     """
         Método PÚBLICO de inserção na árvore
@@ -43,26 +43,125 @@ class BinarySearchTree:
         # 2º caso: inserção recursiva ~> percorrer a árvore recursivamente
         else: self.__insert_node(inserted, self.__root)
 
-        # Exibe a árvore (tosco, para debug)
-        print(f'[ÁRVORE] data: {self.__root.data}, left: {self.__root.left}, right: {self.__root.right}')
-
     """
         Método __privado para inserção de um nodo na árvore
     """
-    def __insert_node(self, node, root):
+    def __insert_node(self, inserted, root):
         # 1º caso: valor do nodo é MENOR que o valor da raiz ~> vai para a ESQUERDA
-        if node.data < root.data:
-            # Se o nodo da esquerda estiver desocupaado, faz aí a inserção
-            if root.left is None: root.left = node
+        if inserted.data < root.data:
+            # Se o nodo da esquerda estiver desocupado, faz aí a inserção
+            if root.left is None: root.left = inserted
+            # Senão, passa a considerar o nodo da esquerda como raiz
+            else: self.__insert_node(inserted, root.left)
+        
         # 2º caso: valor do nodo é MAIOR que o valor da raiz ~> vai para a DIREITA
-        elif node.data > root.data:
-            # Se o nodo da direita esstiver ddesocupado, faz aí a inserção
-            if root.right is None: root.right = node
+        elif inserted.data > root.data:
+            # Se o nodo da direita estiver desocupado, faz aí a inserção
+            if root.right is None: root.right = inserted
+            # Senão, passa a considerar o nodo da direita como raiz
+            else: self.__insert_node(inserted, root.right)
 
-###################################################################
+        # OBSERVAÇÃO: quando inserted.data == root.data (tentativa de inserção
+        # de um valor que já existe na árvore), nada acontece. A tentativa é
+        # ignorada.
+
+    """
+        Método que exibe a árvore recursivamente
+    """
+    def to_str(self, root = None):
+
+        if root is None: root = self.__root
+
+        output = ''
+
+        if root is not None:
+            output += f'[ROOT data: {root.data}]\n'  # \n = Quebra de linha (enter)
+
+            if root.left is not None:
+                output += f'(À esquerda de {root.data}) ' + self.to_str(root.left)
+
+            if root.right is not None:
+                output += f'(À direita de {root.data}) ' + self.to_str(root.right)
+
+        return output
+
+    """
+        PERCURSOS
+    """
+
+    """
+        Método que faz o percurso em-ordem (in-order traversal)
+        1º: visita em-ordem a subárvore esquerda
+        2º: visita a raiz
+        3º: visita em-ordem a subárvore direita
+        Este percurso é utilizado quando se deseja recuperar os elementos
+        da árvore em ordem
+    """
+    def in_order_traversal(self, fnCallback, root = False):
+
+        if root is False: root = self.__root
+
+        if root is not None:
+            self.in_order_traversal(fnCallback, root.left) # 1º
+            fnCallback(root.data)   # 2º
+            self.in_order_traversal(fnCallback, root.right) # 3º
+
+    """
+        Método que faz o percurso pré-ordem (pre-order traversal)
+        1º: visita a raiz
+        2º: visita pré-ordem a subárvore esquerda
+        3º: visita pré-ordem a subárvore direita
+        Este percurso é utilizado quando se deseja copiar a árvore,
+        preservando sua estrutura
+    """
+    def pre_order_traversal(self, fnCallback, root = False):
+
+        if root is False: root = self.__root
+
+        if root is not None:
+            fnCallback(root.data)   # 1º
+            self.pre_order_traversal(fnCallback, root.left) # 2º
+            self.pre_order_traversal(fnCallback, root.right) # 3º
+
+
+######################################################################
 
 arvore = BinarySearchTree()
 
 arvore.insert(43)
 arvore.insert(27)
 arvore.insert(64)
+arvore.insert(36)
+arvore.insert(10)
+arvore.insert(0)
+
+print(arvore.to_str())
+
+em_ordem = []
+
+def insere_em_ordem(val):
+    em_ordem.append(val)
+
+#arvore.in_order_traversal(insere_em_ordem)
+arvore.in_order_traversal(lambda val: em_ordem.append(val))
+#arvore.in_order_traversal(lambda val: print(val))
+
+print('Percurso em-ordem:', em_ordem)
+
+sumario = BinarySearchTree()
+
+sumario.insert('2')
+sumario.insert('1')
+sumario.insert('3')
+sumario.insert('1.1')
+sumario.insert('3.1')
+sumario.insert('2.1')
+sumario.insert('2.1.1')
+
+em_ordem = []
+sumario.in_order_traversal(lambda val: em_ordem.append(val))
+print('Sumário em-ordem:', em_ordem)
+
+pre_ordem = []
+sumario.pre_order_traversal(lambda val: pre_ordem.append(val))
+print('Sumário pré-ordem:', pre_ordem)
