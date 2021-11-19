d0a8ca8349ace3d21ee07b1a9fa84867d22276f3
GabrielHenrique0 <gabrielcaldeira215@gmail.com>
2021-11-19 23:32:45 +0000

    (19/11) Término da árvore binária de busca

--- lib/binary_search_tree.py
+++ lib/binary_search_tree.py
@@ -241,17 +241,44 @@ class BinarySearchTree:
 
         # 4.4: remoção do nodo de grau 2
 
+        # Precisamos encontrar:
+        # a) O MAIOR nodo da subárvore ESQUERDA; *ou*
+        # b) O MENOR nodo da subárvore DIREITA
+
+        # Nossa opção: usar o maior nodo da subávores esquerDA
+        new_root = self.__max_node(root.left)
+        # Ou: new_root = self.__min_node(root.right)
+
+        # Copia o valor do nodo encontrado para o nodo que está
+        # sendo "removido"
+        root.data = new_root.data
+
+        # Exclui o valor duplicado que está na subárvore esuerda
+        # (de onde veio o valor de new_root)
+        root.left = self.__remove_node(root.left, new_root.data)
+        # Ou: root.right = self.__remove_node(root.right, new_root.data)
+
+        return root
 
 ######################################################################
 
 arvore = BinarySearchTree()
 
-arvore.insert(43)
-arvore.insert(27)
+arvore.insert(48)
+arvore.insert(15)
+arvore.insert(71)
+arvore.insert(1)
+arvore.insert(29)
+arvore.insert(57)
+arvore.insert(80)
+arvore.insert(13)
+arvore.insert(19)
+arvore.insert(37)
+arvore.insert(51)
 arvore.insert(64)
-arvore.insert(36)
-arvore.insert(10)
-arvore.insert(0)
+arvore.insert(23)
+arvore.insert(21)
+arvore.insert(25)
 
 print(arvore.to_str())
 
@@ -286,6 +313,7 @@ print('Sumário pré-ordem:', pre_ordem)
 
 pre_ordem = []
 arvore.pre_order_traversal(lambda val: pre_ordem.append(val))
+
 print('Árvore pré-ordem:', pre_ordem)
 
 pos_ordem = []
@@ -297,3 +325,38 @@ existe51 = arvore.exists(51)
 existe64 = arvore.exists(64)
 print(f'36: {existe36}, 51: {existe51}, 64: {existe64}')
 
+# Teste de exclusão
+
+em_ordem = []
+arvore.in_order_traversal(lambda val: em_ordem.append(val))
+print('Antes da exclusão do 29: ', em_ordem)
+
+arvore.remove(29)
+
+em_ordem = []
+arvore.in_order_traversal(lambda val: em_ordem.append(val))
+print('Depois da exclusão do 29: ', em_ordem)
+
+arvore.remove(21)
+
+em_ordem = []
+arvore.in_order_traversal(lambda val: em_ordem.append(val))
+print('Depois da exclusão do 21 (grau 0): ', em_ordem)
+
+arvore.remove(1)
+
+em_ordem = []
+arvore.in_order_traversal(lambda val: em_ordem.append(val))
+print('Depois da exclusão do 1 (grau 1): ', em_ordem)
+
+arvore.remove(48)
+
+em_ordem = []
+arvore.in_order_traversal(lambda val: em_ordem.append(val))
+print('Depois da exclusão do 48 (grau 2, raiz): ', em_ordem)
+
+# Observando qual valor se tornou a nova raiz
+# Fazendo um percurso pré-ordem e verificando o primeiro valor retornado
+pre_ordem = []
+arvore.pre_order_traversal(lambda val: pre_ordem.append(val))
+print('Novo nodo raiz tem o valor', pre_ordem[0])
